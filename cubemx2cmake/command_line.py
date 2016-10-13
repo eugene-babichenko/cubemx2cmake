2fcac39a97bcd8ccaef5cd8454178e89f904370e
Yevhenii Babichenko <eugene.babichenko@gmail.com>
2016-10-13 12:25:58 +0300

    Fixed PEP8 E401 and W293 in command_line.py; Fixed E302, E101, W191, E131 for setup.py

--- cubemx2cmake/command_line.py
+++ cubemx2cmake/command_line.py
@@ -1,5 +1,6 @@
 from pkg_resources import resource_filename
-import sys, os
+import sys
+import os
 from configparser import ConfigParser
 from string import Template
 
@@ -16,7 +17,7 @@ def main():
         "STM32F4xx": "-mcpu=cortex-m4 -mfpu=hard -mfloat-abi=fpv4-sp-d16",
         "STM32F7xx": "-mcpu=cortex-m7 -mfpu=hard -mfloat-abi=fpv5-sp-d16"
     }
-    
+
     try:
         cube_file = sys.argv[1]
     except Exception:
@@ -30,7 +31,7 @@ def main():
         else:
             print("No input file was specified!")
             exit(0)
-    
+
     cube_config = ConfigParser()
     try:
         cube_config.read_string(u"[section]\n"+open(cube_file).read())
@@ -38,7 +39,7 @@ def main():
         print("Input file doesn't exist, is broken or access denied.")
         exit(0)
     cube_config = dict(cube_config["section"])
-    
+
     try:
         mcu_name = cube_config["mcu.name"]
         mcu_username = cube_config["mcu.username"]
@@ -54,12 +55,12 @@ def main():
         "MCU_LINE": mcu_username[:9]+"x"+mcu_name[13],
         "MCU_LINKER_SCRIPT": mcu_username+"_FLASH.ld"
     }
-    
+
     toolchain_params = {
         "MCU_LINKER_SCRIPT": mcu_username+"_FLASH.ld",
         "MCU_ARCH": architecture[mcu_family]
     }
-    
+
     try:
         with open(resource_filename(__name__, "CMakeLists.txt.template"), "r") as cmakelists_template:
             with open("CMakeLists.txt", "w") as cmakelists:
--- setup.py
+++ setup.py
@@ -1,25 +1,28 @@
 from setuptools import setup
 
+
 def readme():
     with open('README.rst') as f:
         return f.read()
 
 setup(
-	name='cubemx2cmake',
-	version='1.0.5',
-	description='Command line tool to generate CMakeLists.txt from STM32CubeMX projects.',
-	long_description=readme(),
-	classifiers=[
-		'Topic :: Software Development :: Code Generators',
-		'Topic :: Utilities'
-	],
-	keywords='st stm32 cube cubemx stm32cubemx cmake code generator',
-	url='https://github.com/eugene-babichenko/cubemx2cmake',
-	author='Yevhenii Babichenko',
-	author_email='eugene.babichenko@gmail.com',
-	license='MIT',
-	packages=['cubemx2cmake'],
-	zip_safe=False,
-	include_package_data=True,
-	entry_points={'console_scripts': ['cubemx2cmake=cubemx2cmake.command_line:main']}
+    name='cubemx2cmake',
+    version='1.0.5',
+    description='Command line tool to generate CMakeLists.txt from STM32CubeMX projects.',
+    long_description=readme(),
+    classifiers=[
+        'Topic :: Software Development :: Code Generators',
+        'Topic :: Utilities'
+    ],
+    keywords='st stm32 cube cubemx stm32cubemx cmake code generator',
+    url='https://github.com/eugene-babichenko/cubemx2cmake',
+    author='Yevhenii Babichenko',
+    author_email='eugene.babichenko@gmail.com',
+    license='MIT',
+    packages=['cubemx2cmake'],
+    zip_safe=False,
+    include_package_data=True,
+    entry_points={
+        'console_scripts': ['cubemx2cmake=cubemx2cmake.command_line:main']
+    }
 )

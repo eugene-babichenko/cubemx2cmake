78050b0e4d29478be52f16361545245e645fb0bd
Yevhenii Babichenko <eugene.babichenko@gmail.com>
2017-02-25 14:00:41 +0200

    Written the testing script for generator

--- .travis/script.sh
+++ .travis/script.sh
@@ -1,1 +1,29 @@
-# test
+#!/usr/bin/env bash
+
+# For projects that can be successfully built
+set -e
+for dir in ./test-data/should-success/*/
+do
+    cd ${dir}
+    cubemx2cmake
+    mkdir build
+    cd build
+    cmake .. -D CMAKE_TOOLCHAIN_FILE=../STM32Toolchain.cmake
+    ls
+    make
+    cd ../..
+done
+
+# For projects, that should not be generated
+set +e
+for dir in ./test-data/should-not-generate/*/
+do
+    cd ${dir}
+    cubemx2cmake
+    if [ $? -eq 0 ]
+    then
+        echo "Generator should fail on this project"
+        exit 1
+    fi
+    cd ..
+done
